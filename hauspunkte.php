937fd16a662a1a28ad57cd45ab64f23046f6b5c6
Alexandra <38242434+Ales12@users.noreply.github.com>
2020-09-13 16:19:03 +0200

    Update hauspunkte.php

--- hauspunkte.php
+++ hauspunkte.php
@@ -8,59 +8,45 @@ define("IN_MYBB", 1);
 error_reporting ( -1 );
 ini_set ( 'display_errors', true );
 require("global.php");
-global $db, $mybb;
+global $db, $mybb, $templates, $housepoints, $all, $housepoints_bit, $page,  $user ;
 
-add_breadcrumb("Hauspunkte", "hauspunkte.php");
+    $group_sect = $db->simple_select("usergroups", "gid,title", "gid IN ('" . str_replace(',', '\',\'', $mybb->settings['hp_groups']) . "')");
 
+    while ($house = $db->fetch_array($group_sect)) {
 
-
-$group_sect = $db->simple_select("usergroups", "gid,title", "gid IN ('".str_replace(',', '\',\'', $mybb->settings['hp_groups'])."')");
-
-while($house = $db->fetch_array($group_sect)){
-
-    $select = $db->query("SELECT *
-    FROM ".TABLE_PREFIX."users u
-    LEFT JOIN ".TABLE_PREFIX."usergroups ug
+        $select = $db->query("SELECT *
+    FROM " . TABLE_PREFIX . "users u
+    LEFT JOIN " . TABLE_PREFIX . "usergroups ug
     ON u.usergroup = ug.gid
-    LEFT JOIN ".TABLE_PREFIX."userfields uf
+    LEFT JOIN " . TABLE_PREFIX . "userfields uf
     ON uf.ufid = u.uid
     WHERE u.usergroup = $house[gid]
-    Order by username ASC
+    Order by hp_points DESC, username ASC
     ");
 
-    $postfaktor = $mybb->settings['hp_post'];
+        $all = 0;
 
-    $wantedfaktor = $mybb->settings['wanted_points'];
+        $student = "";
 
-    $all = 0;
+        while ($row = $db->fetch_array($select)) {
 
-$student = "";
-   
-    while($row = $db->fetch_array($select)){
-        $postpoints = ($row['postnum'] * $postfaktor) + $row['hp_points'] + 15;
-        $username = format_name ($row['username'], $row['usergroup'], $row['displaygroup']);
-        $user = build_profile_link ($username, $row['uid']);
+            $username = format_name($row['username'], $row['usergroup'], $row['displaygroup']);
+            $user = build_profile_link($username, $row['uid']);
+
+            $postpoints = $row['hp_points'];
+            $student .= $user . " - " . $postpoints . " Hauspunkte <br />";
+
+            $all = $all + $postpoints;
 
 
-        if($mybb->settings['hp_quest'] == 1){
-            $postpoints = $postpoints + $row['questpoints'];
         }
-
-        $fid = $row['fid'.intval($mybb->settings['hp_gesuche_adop']).''];
-        if(preg_match("/a/i", "$fid")){
-            $postpoints = $postpoints + $wantedfaktor;
-        }
-     $student .= $user." - ". $postpoints." Hauspunkte <br />";
-        
-        $all = $all + $postpoints;
+        $all = number_format($all, '0', ',', '.');
+        eval("\$housepoints_bit .= \"" . $templates->get("housepoints_bit") . "\";");
     }
 
-    eval("\$housepoints_bit .= \"".$templates->get("housepoints_bit")."\";");
-}
 
+    eval("\$page = \"" . $templates->get("housepoints") . "\";"); // Hier wird das erstellte Template geladen
+    output_page($page);
 
-
-eval("\$page = \"".$templates->get("housepoints")."\";"); // Hier wird das erstellte Template geladen
-output_page($page);
 
 ?>
